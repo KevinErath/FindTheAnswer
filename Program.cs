b9ccf5776bb8abf0064c63ce04df1647a356b6ba
Kevin Erath <KevinErath@users.noreply.github.com>
2015-06-27 23:32:14 +0200

    First commit

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+internal class Program
+{
+    private static void Main()
+    {
+        var numbers = ReadNumbersFromCmd();
+        var answer = FindTheAnswer(numbers);
+        PrintNumbers(answer);
+    }
+
+    public static IEnumerable<int> ReadNumbersFromCmd()
+    {
+        while (true)
+        {
+            var line = Console.ReadLine();
+            yield return int.Parse(line);
+        }
+    }
+
+    private static IEnumerable<int> FindTheAnswer(IEnumerable<int> numbers)
+    {
+        return numbers.TakeWhile(x => x != 42);
+    }
+
+    private static void PrintNumbers(IEnumerable<int> numbers)
+    {
+        foreach (var number in numbers)
+        {
+            Console.WriteLine(number);
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # FindTheAnswer
+
+Small application which searches THE Answer to all questions.
+
+Read more about this on:
+http://www.code-whisperer.de/2015/06/14/eva
+http://www.spoj.com/problems/TEST
